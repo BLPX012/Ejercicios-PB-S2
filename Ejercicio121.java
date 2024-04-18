bcda0cbbe4253f6d9067ec7f6af6f8cbb8d692e2
Braian Llamas Peralta <braian.llamasperalta@unicolombo.edu.co>
2024-04-17 20:15:58 -0500

    Cambio, ejercicio 121

--- Ejercicio121.java
+++ Ejercicio121.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class Ejercicio121 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Digite 5 números: ");
+
+        int suma = 0;
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            int numero = scanner.nextInt();
+            suma += numero;
+        }
+
+        System.out.println("La suma total de los números es: " + suma);
+        scanner.close();
+    }
 }
