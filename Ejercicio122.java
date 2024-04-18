0b0c1f3552fb7c0ad4bb4335d604279fb2c1ac8a
Braian Llamas Peralta <braian.llamasperalta@unicolombo.edu.co>
2024-04-17 20:19:43 -0500

    Cambio Ejercicio 122

--- Ejercicio122.java
+++ Ejercicio122.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
 public class Ejercicio122 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Ingrese números (ingrese 0 para finalizar):");
+
+        int suma = 0;
+
+        int numero;
+        do {
+            System.out.print("Número: ");
+            numero = scanner.nextInt();
+            suma += numero;
+        } while (numero != 0);
+
+        // Mostrar la suma total de los números
+        System.out.println("La suma total de los números es: " + suma);
+        scanner.close();
+    }
 }
